e2d8b04ddda1226259492da16f96e77fb3660ccc
Shenkrad <cristian4_mejo@hotmail.com>
2022-12-04 13:12:48 +0100

    Added Triggers to move deactivated platforms.

--- Source/PuzzlePlatforms/MovingPlatform.cpp
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -18,21 +18,49 @@ void AMovingPlatform::BeginPlay()
     {
         SetReplicates(true);
         SetReplicatingMovement(true);
-    }  
+    }
+
+    GlobalStartLocation = GetActorLocation();
+    GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
 }
 
 void AMovingPlatform::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
 
-    if (HasAuthority())
+    if (ActiveTriggers > 0)
     {
-        FVector Location = GetActorLocation();
-        FVector GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
-        FVector Direction = (GlobalTargetLocation - Location).GetSafeNormal();
-        Location += Speed * Direction * DeltaTime;
-        SetActorLocation(Location);
+        if (HasAuthority())
+        {
+            FVector Location = GetActorLocation();
+            float JourneyLength = (GlobalTargetLocation - GlobalStartLocation).Size();
+            float JourneyTravelled = (Location - GlobalStartLocation).Size();
+
+            if (JourneyTravelled >= JourneyLength)
+            {
+                FVector Swap = GlobalStartLocation;
+                GlobalStartLocation = GlobalTargetLocation;
+                GlobalTargetLocation = Swap;
+            }
+            
+
+            FVector Direction = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
+            Location += Speed * Direction * DeltaTime;
+            SetActorLocation(Location);
+        }
     }
-    
+}
+
+void AMovingPlatform::AddActiveTrigger()
+{
+    ActiveTriggers++;
+}
+
+void AMovingPlatform::RemoveActiveTrigger()
+{   
+    if (ActiveTriggers > 0)
+    {
+        ActiveTriggers--;
+    }
 }
 
